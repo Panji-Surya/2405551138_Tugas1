45d60135a96247a3183154566b7e54958dfca361
Madepan <82923125+Panji-Surya@users.noreply.github.com>
2025-09-23 22:41:41 +0800

    Update kalkulator.php

--- kalkulator.php
+++ kalkulator.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator - 2405551138 I Made Bagus Panji Surya Permana</title>
+    <title>Kalkulators - 2405551138 I Made Bagus Panji Surya Permana</title>
 </head>
 <body>
 
